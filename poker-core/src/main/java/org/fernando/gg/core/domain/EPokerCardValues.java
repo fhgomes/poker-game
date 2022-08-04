8152245187f10226264b11c0370e778b1447e598
Fernando Gomes <fernandogomes_ti@hotmail.com>
2022-08-04 15:39:08 -0300

    simple start and check junit test

---
+++ poker-core/src/main/java/org/fernando/gg/core/domain/EPokerCardValues.java
@@ -0,0 +1,50 @@
+package org.fernando.gg.core.domain;
+
+import java.util.stream.Stream;
+
+public enum EPokerCardValues {
+
+	A("A", "Ace", 1),
+	TWO("2", "2", 2),
+	THREE("3", "3", 3),
+	FOUR("4", "4", 4),
+	FIVE("5", "5", 5),
+	SIX("6", "6", 6),
+	SEVEN("7", "7", 7),
+	EIGHT("8", "8", 8),
+	NINE("9", "9", 9),
+	TEN("10", "10", 10),
+	JACK("J", "Jack", 11),
+	QUEEN("Q", "Queen", 12),
+	KING("K", "King", 13);
+
+	private final String visualKey;
+	private final String visualName;
+	private final int value;
+
+	EPokerCardValues(String visualKey, String visualName, int value) {
+		this.visualKey = visualKey;
+		this.visualName = visualName;
+		this.value = value;
+	}
+
+	public EPokerCardValues getByValue(int value) {
+		return Stream
+			.of(values())
+			.filter(card -> card.value == value)
+			.findFirst()
+			.orElseThrow(() -> new RuntimeException("CardValue invalid: "+value));
+	}
+
+	public String getVisualKey() {
+		return visualKey;
+	}
+
+	public String getVisualName() {
+		return visualName;
+	}
+
+	public int getValue() {
+		return value;
+	}
+}
---
+++ poker-core/src/test/java/org/fernando/gg/core/domain/EPokerCardValuesTest.java
@@ -0,0 +1,48 @@
+package org.fernando.gg.core.domain;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class EPokerCardValuesTest {
+
+	@Test
+	@DisplayName("When given key cards, return correct value")
+	void shouldGetCorrectKeyValues() {
+		assertAll(
+			() -> assertEquals(1, EPokerCardValues.A.getValue()),
+			() -> assertEquals(11, EPokerCardValues.JACK.getValue()),
+			() -> assertEquals(12, EPokerCardValues.QUEEN.getValue()),
+			() -> assertEquals(13, EPokerCardValues.KING.getValue())
+		);
+	}
+
+	@Test
+	@DisplayName("When given key cards, return correct char")
+	void shouldGetCorrectKey() {
+		assertAll(
+			() -> assertEquals("A", EPokerCardValues.A.getVisualKey()),
+			() -> assertEquals("J", EPokerCardValues.JACK.getVisualKey()),
+			() -> assertEquals("Q", EPokerCardValues.QUEEN.getVisualKey()),
+			() -> assertEquals("K", EPokerCardValues.KING.getVisualKey())
+		);
+	}
+
+	@DisplayName("When given number cards, return correct value")
+	@ParameterizedTest
+	@ValueSource(ints = {2, 3, 4, 5, 6, 7, 8, 9, 10})
+	void shouldGetCorrectNumberValue(int cardValue) {
+		assertEquals(cardValue,EPokerCardValues.A.getByValue(cardValue).getValue());
+	}
+
+	@DisplayName("When given number cards, return correct key")
+	@ParameterizedTest
+	@ValueSource(ints = {2, 3, 4, 5, 6, 7, 8, 9, 10})
+	void shouldGetCorrectNumberKey(int cardValue) {
+		assertEquals(String.valueOf(cardValue), EPokerCardValues.A.getByValue(cardValue).getVisualKey());
+	}
+}
