2f9c31cb4580a9face98f5a3d07e1af099c4a10d
Fernando Gomes <fernandogomes_ti@hotmail.com>
2022-08-04 14:45:54 -0300

    fix missconfig , ready to use

---
+++ poker-core/src/main/java/org/fernando/gg/core/PingResource.java
@@ -0,0 +1,18 @@
+package org.fernando.gg.core;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/ping")
+public class PingResource {
+
+	@GetMapping
+	public String doPing() {
+		return "Pong";
+	}
+
+}
